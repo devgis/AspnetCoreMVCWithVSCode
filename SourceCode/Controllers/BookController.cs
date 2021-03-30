04551c1871e005b5351348817ab6a34459be2758
devgis <devgis@163.com>
2021-03-30 21:18:38 +0800

    Add book controller
    Add book controller

---
+++ SourceCode/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Temp.Models;
+
+namespace Temp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ILogger<BookController> _logger;
+
+        public BookController(ILogger<BookController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Book Home Page";
+            var books=new List<BookItem>();
+            for(int i=0;i<100;i++)
+            {
+                books.Add(new BookItem{ID="B"+i,Name="Name"+i,Autor="Autor"+i});
+            }
+            return View(books);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SourceCode/Models/BookItem.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Temp.Models
+{
+    public class BookItem
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+        public string Autor { get; set; }
+    }
+}
